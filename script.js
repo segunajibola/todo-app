d4e1c436417b84d7469ffbef26e37f72f833b8e0
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-18 01:59:35 +0100

    edit todos

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todos = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const todoText = input.value;
+
+    if(todoText) {
+        const todoEl = document.createElement("li");
+        todoEl.innerText = todoText;
+
+        todoEl.addEventListener("click", () => {
+            todoEl.classList.toggle("completed")
+        })
+        todos.appendChild(todoEl);
+
+        input.value = "";
+    }
+});
