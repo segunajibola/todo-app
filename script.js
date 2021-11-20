e741fad58d377271ec93d3632607909e76ed1c27
Segun Ajibola <ajbl.segun@gmail.com>
2021-11-20 19:16:02 +0100

    edit todos

--- script.js
+++ script.js
@@ -6,22 +6,30 @@ const todos = JSON.parse(localStorage.getItem("todos"));
 
 if(todos) {
     todos.forEach(todo => {
-
-    })
-    
+        addTodo(todo)
+    });
 }
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
-    addTodo()
+    addTodo();
 });
 
-function addTodo() {
-    const todoText = input.value;
+function addTodo(todo) {
+    let todoText = input.value;
+
+    if(todo) {
+        todoText = todo.text
+    }
 
     if(todoText) {
         const todoEl = document.createElement("li");
+
+        if (todo && todo.completed) {
+            todoEl.classList.add("completed");
+        }
+
         todoEl.innerText = todoText;
 
         todoEl.addEventListener("click", () => {
